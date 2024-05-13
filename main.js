91a0e1e564cb0aaf693cb7a53e948660b857d50b
Isaiah <emmonsis717@northwested.org>
2024-05-13 10:38:52 -0400

    dfghjk

--- main.js
+++ main.js
@@ -3,3 +3,36 @@
 // JS Arrow Functions Practice
 
 // View the markdown (.md) file in VS Code for instructions
+
+
+
+
+
+// const firstNum = 4;
+// const secondNum = 5;
+
+// const product = (num1, num2) => num1 * num2;
+
+
+// console.log(`The product of ${firstNum} and ${secondNum} is: ${product(firstNum, secondNum)}`);
+
+// const firstName = 'Mikey';
+
+// const welcomeMsg = fName => ` Welcome to Career Tech, ${fName}`;
+
+// console.log(welcomeMsg(firstName));
+
+
+
+
+
+
+const firstName = 'Tania';
+const lastName = 'Rascia';
+const age = 17;
+
+
+
+const studentBio = (fName, lName, userAge) => `Hello! My name is ${fName} ${lName} and I am ${userAge} years old.`;
+
+console.log(studentBio(firstName, lastName, age));
